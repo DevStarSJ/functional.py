f2d001f2bd8739bae32a76ecca97ff9f70c492a9
Seokjoon Yun <sj@zigbang.com>
2017-07-02 16:28:19 +0900

    is_true, is_sequence, is_dict, each

--- functional.py
+++ functional.py
@@ -1,19 +1,55 @@
-def isFalse(obj):
-    return not obj
+"""functional utilities
+"""
 
-if __name__ == "__main__":
-    import unittest
+def is_false(arg):
+    """Check arg is False.
+    It can also be used to return the not value of arg.
 
-    class UnitTest(unittest.TestCase):
-        def isFalse_test(self):
-            self.assertEqual(isFalse(0),True)
-            self.assertEqual(isFalse(1),False)
-            self.assertEqual(isFalse(-1),False)
-            self.assertEqual(isFalse(True),False)
-            self.assertEqual(isFalse(False),True)
-            self.assertEqual(isFalse(None),True)
-            self.assertEqual(isFalse(""),True)
-            self.assertEqual(isFalse("a"),False)
+    arg: anything
+    return: True of False
+    """
+    return not arg
 
-    unittest.main()
+def is_true(arg):
+    """Check arg is True.
+
+    arg: anything
+    return: True of False
+    """
+    return not is_false(arg)
+
+def is_sequence(arg):
+    """Check arg is sequence (list, tuple, string)
+
+    arg: anything
+    return: True of False
+    """
+    t = type(arg)
+    return t is list or t is tuple or t is str
+
+def is_dict(arg):
+    """Check arg is dict
+
+    arg: anything
+    return: True of False
+    """
+    t = type(arg)
+    return t is dict
+
+def each(seq, func):
+    """Run func for all value of seq
+
+    args:
+        seq: sequence or dict
+        func: function with one argument 
+    return: None
+    """
+    if (is_false(seq)): return
+
+    if is_sequence(seq):
+        for i in seq:
+            func(i)
+    elif is_dict(seq):
+        for i in seq.values():
+            func(i)
 
--- test.py
+++ test.py
@@ -1,16 +1,74 @@
 import unittest
 import functional as F
 
-class UnitTest(unittest.TestCase):
-    def test_isFalse(self):
-        self.assertEqual(F.isFalse(None),True)
-        self.assertEqual(F.isFalse(0),True)
-        self.assertEqual(F.isFalse(1),False)
-        self.assertEqual(F.isFalse(-1),False)
-        self.assertEqual(F.isFalse(True),False)
-        self.assertEqual(F.isFalse(False),True)
-        self.assertEqual(F.isFalse(""),True)
-        self.assertEqual(F.isFalse("a"),False)
+_list = [1,2,3,4]
+_tuple = (1,2,3)
+_str = "seokjoon.yun"
+_dict = { "id": 1, "name": "seokjoon.yun" }
+_dict_values = [1, "seokjoon.yun"]
+_dict_keys = ["id", "name"]
 
+class Class1:
+    def __init__(self):
+        pass
+
+_class = Class1()
+
+class UnitTest(unittest.TestCase):
+    def test_is_false(self):
+        self.assertEqual(F.is_false(None),True)
+        self.assertEqual(F.is_false(0),True)
+        self.assertEqual(F.is_false(1),False)
+        self.assertEqual(F.is_false(-1),False)
+        self.assertEqual(F.is_false(True),False)
+        self.assertEqual(F.is_false(False),True)
+        self.assertEqual(F.is_false(""),True)
+        self.assertEqual(F.is_false("a"),False)
+
+    def test_is_true(self):
+        self.assertEqual(F.is_true(None),False)
+        self.assertEqual(F.is_true(0),False)
+        self.assertEqual(F.is_true(1),True)
+        self.assertEqual(F.is_true(-1),True)
+        self.assertEqual(F.is_true(True),True)
+        self.assertEqual(F.is_true(False),False)
+        self.assertEqual(F.is_true(""),False)
+        self.assertEqual(F.is_true("a"),True)
+
+    def test_is_sequence(self):
+        self.assertEqual(F.is_sequence(_list),True)
+        self.assertEqual(F.is_sequence(_tuple),True)
+        self.assertEqual(F.is_sequence(_str),True)
+        self.assertEqual(F.is_sequence(_dict),False)
+        self.assertEqual(F.is_sequence(_class),False)
+
+    def test_is_dict(self):
+        self.assertEqual(F.is_dict(_list),False)
+        self.assertEqual(F.is_dict(_tuple),False)
+        self.assertEqual(F.is_dict(_str),False)
+        self.assertEqual(F.is_dict(_dict),True)
+        self.assertEqual(F.is_dict(_class),False)
+
+    def test_each(self):
+        self.assertEqual(F.is_dict(_list),False)
+        self.assertEqual(F.is_dict(_tuple),False)
+
+        _l = []
+        F.each(_list, lambda x: _l.append(x))
+        self.assertEqual(_list,_l)
+
+        _l = []
+        F.each(_tuple, lambda x: _l.append(x))
+        self.assertEqual(list(_tuple),_l)
+
+        _l = []
+        F.each(_str, lambda x: _l.append(x))
+        self.assertEqual(list(_str),_l)
+
+        _l = []
+        F.each(_dict, lambda x: _l.append(x))
+        self.assertEqual(_dict_values,_l)   
+    
+        
 if __name__ == "__main__":
     unittest.main()
